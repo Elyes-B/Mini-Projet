f2cc410c492babb07a3034389af444ce7364d45a
Ilyes Belkahia <elyesbelkahia22@gmail.com>
2025-11-29 16:01:50 +0100

    added db connect

---
+++ db.php
@@ -0,0 +1,14 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');
+define('DB_PASS','');
+define ('DB_NAME','TechZone');
+
+try {
+    $pdo = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (Exception $e)
+{
+die('Erreur : ' . $e->getMessage());
+}
+?>
--- search.php
+++ search.php
@@ -1,4 +1,18 @@
 <?php 
 $template = 'search';
+
+$searchText=isset($_GET['searchText']) ? trim($_GET['searchText']) : '';
+$categoryText=isset($_GET['category']) ? trim($_GET['category']) : 'all';
+if ($categoryText === 'all') {
+    $category = '%';
+} else {
+    $stmt = $pdo->prepare("SELECT id FROM Categorie WHERE cat_name LIKE :category");
+    $stmt->execute(['category' => $categoryText]);
+    $category = $stmt->fetch()['id'];
+}
+$products=$pdo->prepare("SELECT * FROM Produit WHERE (prd_name LIKE :searchText OR prd_description LIKE :searchText OR category_id LIKE :category)");
+$products->execute(['searchText' => "%$searchText%", 'category' => $category]);
+$products = $products->fetchAll();
+var_dump($products);
 include 'layout.phtml';
 ?>
