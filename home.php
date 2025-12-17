dd5dc2b7094f7f723ebf93a4ff8da13aae4b08a9
yassmine ben kaabia <yassminebenkaabia@gmail.com>
2025-12-17 17:53:25 +0100

    elyes

--- home.php
+++ home.php
@@ -1,12 +1,8 @@
 <?php
 
 include 'db.php';
-$products = [
-['id' => 1, 'name' => 'Huawei MediaPad', 'price' => 225, 'oldPrice' => 300, 'badge' => '-25%', 'category' => 'featured'],
-['id' => 2, 'name' => 'Sony MDRZX310W', 'price' => 379, 'oldPrice' => null, 'badge' => null, 'category' => 'best-rated'],
-['id' => 3, 'name' => 'Canon STM Kit', 'price' => 225, 'oldPrice' => null, 'badge' => null, 'category' => 'featured'],
-['id' => 4, 'name' => 'Lenovo IdeaPad', 'price' => 379, 'oldPrice' => null, 'badge' => 'new', 'category' => 'on-sale']
-    ];
+$products =$pdo->query('SELECT * FROM produit');
+$pro=$products->fetchAll();
 $template = 'home';
 include 'layout.phtml';
 ?>
